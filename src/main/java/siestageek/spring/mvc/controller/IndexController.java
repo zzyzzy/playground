70658abe4df4e7f8b9d51f706d5c2136626283bb
zzyzzy <zzyzzy@xyz.com>
2021-02-10 15:37:19 +0900

    팀장의 첫번째 커밋

---
+++ src/main/java/siestageek/spring/mvc/controller/IndexController.java
@@ -0,0 +1,14 @@
+package siestageek.spring.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
